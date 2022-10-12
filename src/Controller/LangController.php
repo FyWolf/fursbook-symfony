b66d5dbde0dfa9bb3dcac2ffbc557d986a7d62d0
FyWolf <alanpoux@gmail.com>
2022-10-12 23:56:06 +0200

    Fixed a missing line

--- src/Controller/LangController.php
+++ src/Controller/LangController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
